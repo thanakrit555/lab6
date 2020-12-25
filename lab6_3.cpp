4e9f43cbdaf75d812630e9e5e5e37ea7f1ca7e78
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-12-25 14:52:01 +0700

    Update lab6_3.cpp

--- lab6_3.cpp
+++ lab6_3.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	int y = x-1;
+	char result = y;
+	if(y >=64 and y <=90){
+		result =y;
+	}else{
+		result = '0' ;
+	}
+	if(y==64){
+		result = 90;
+	}
+	return result;
 }
 
 int main(){
