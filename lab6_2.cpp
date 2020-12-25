1f1d18d9b4929f0c2bd1ace335dbd8bb1649b68b
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2020-12-25 16:01:36 +0700

    Update lab6_2.cpp

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -16,3 +16,37 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+#include<string>
+using namespace std;
+
+int main(){
+    string name,ans,movie,idiom;
+    int ID,GEAR;
+  
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?"<< endl;
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<< name <<" is a really cool name."<<endl;
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?"<<endl;
+    cout << name <<": ";
+    cin >> ID;
+    GEAR = (ID/10000000)-12;
+    cout << "Fahsai: I think you may be GEAR "<<GEAR<<". I have a free movie ticket for you."<<endl;
+    cout<<"Fahsai: Let's go to the cinema together!!!"<<endl;
+    cout<<"Fahsai: What movie do you want to watch?"<<endl;
+    cout << name <<": ";
+    cin.ignore();
+    getline(cin,movie);
+    cout << "Fahsai: So....which day are you free to go with me?"<<endl;
+    cout << name <<": ";
+    getline(cin,ans);
+    cout <<"Fahsai: "<< ans <<"....that is OK!!! I'm looking forward to watching "<<movie<<" with you."<<endl;
+    cout << name <<": ";
+    cin >> idiom;
+    cout << "Fahsai: 555+ see you "<<ans<<". Bye Bye \\(^ ^)/ ";    
+}
